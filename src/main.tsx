1db58e0cf0903b7c117dd06cc5b73b40f214c7c0
Darabos Benjamin <darabosbeni@gmail.com>
2025-04-24 01:18:04 +0200

    Bugfixes

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
+import MyRecipeBook from "./MyRecipeBook.tsx";
 import "./lib/i18n.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <MyRecipeBook />
   </StrictMode>
 );
